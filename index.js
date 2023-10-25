b8f19ee1ea018eeaab76ea41e38989fd033d38de
IsmaylHossain <ismaylhossain786@gmail.com>
2023-10-25 23:27:53 +0600

    server connection to client complete

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ async function run() {
         res.send(result)
     })
 
+
+    
     app.get('/product', async(req,res)=>{
         const cursor = productCollection.find();
         const result = await cursor.toArray();
@@ -47,7 +49,7 @@ async function run() {
     })
 
 
-    
+
     app.get('/product/:id', async(req,res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
