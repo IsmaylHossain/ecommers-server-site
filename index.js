6b07a886b4b0d423867f457cb8181b1daac74057
IsmaylHossain <ismaylhossain786@gmail.com>
2023-10-25 23:27:09 +0600

    server connection complete

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ async function run() {
         res.send(result)
     })
 
+
+    
     app.get('/product/:id', async(req,res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
